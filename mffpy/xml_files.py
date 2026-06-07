c9d72643a9e5a902c5c8bb002c8111e77a2c9aa8
myd7349 <myd7349@gmail.com>
2026-06-07 16:16:24 +0800

    Test

--- mffpy/xml_files.py
+++ mffpy/xml_files.py
@@ -430,9 +430,19 @@ class Patient(XML):
     _default_filename = 'subject.xml'
 
     _type_converter = {
-        'string': str,
+        'string': lambda x: x if x is not None else "",
         None: lambda x: x
     }
+
+    def _parse_field_data(self, data):
+        data_type = data.get('dataType')
+        if data_type in self._type_converter:
+            convert = self._type_converter[data_type]
+        else:
+            warnings.warn(f"unknown type converter for data type '{data_type}'.")
+            convert = self._type_converter[None]
+
+        return convert(data.text)
 
     @cached_property
     def fields(self):
@@ -442,9 +452,7 @@ class Patient(XML):
             Unknown field with tag {self.nsstrip(field.tag)}"""
             name = self.find('name', field).text
             data = self.find('data', field)
-            convert = self._type_converter.get(data.get('dataType'), self._type_converter[None])
-            data = convert(data.text)
-            ans[name] = data
+            ans[name] = self._parse_field_data(data)
         return ans
 
     @classmethod
